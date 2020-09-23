2131735fc18da3bb76b0edac8df1087d356101d8
RyoMurata1025 <lagoon1025.work@gmail.com>
2020-09-23 19:31:00 +0900

    ビューに感情の選択肢反映

--- app/models/motivation.rb
+++ app/models/motivation.rb
@@ -1,7 +1,6 @@
 class Motivation < ActiveHash::Base
 
   self.data = [
-               {id: 0, name: '--'}, {id: 1, name: '楽しい'}, {id: 2, name: '普通'}, 
-               {id: 3, name: '悲しい'}
+               {id: 0, name: '楽しい'}, {id: 1, name: '普通'}, {id: 2, name: '悲しい'}
               ]
 end
