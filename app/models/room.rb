7ea05807778843c4afcf09644a02a9c79c61dada
RyoMurata1025 <lagoon1025.work@gmail.com>
2020-10-05 01:11:19 +0900

    ページ上部の空白削除

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,8 +1,13 @@
 class Room < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :motivation
+
   has_many :room_users
   has_many :users, through: :room_users, dependent: :destroy
   has_many :messages, dependent: :destroy  
+
   
   validates :name, presence: true
+  validates :motivation_id, presence: true
 
 end
