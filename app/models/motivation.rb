160c44b5e2b17e45c6c76dd4c01d883c95f0f879
RyoMurata1025 <lagoon1025.work@gmail.com>
2020-10-20 09:36:32 +0900

    感情の選択肢の追加とpoint設定

--- app/models/motivation.rb
+++ app/models/motivation.rb
@@ -1,8 +1,10 @@
 class Motivation < ActiveHash::Base
 
   self.data = [
-    {id: -1, name: '悲しい'},
-    {id: 0, name: '普通'}, 
-    {id: 1, name: '楽しい'}, 
+    {id: 0, name: '普通', point: 0}, 
+    {id: 1, name: '楽しい', point: 1},
+    {id: 2, name: 'とても楽しい', point: 2},
+    {id: 3, name: '悲しい', point: -1},
+    {id: 4, name: 'とても悲しい', point: -2},
    ]
 end
