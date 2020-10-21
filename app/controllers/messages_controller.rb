52a25290cd838cfdeecc07a9d6e766d7cb4acb97
RyoMurata1025 <lagoon1025.work@gmail.com>
2020-10-21 12:32:29 +0900

    motivation_idのpointによって表示される文言を変える

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,12 +3,17 @@ class MessagesController < ApplicationController
     @message = Message.new
     @room = Room.find(params[:room_id])
     @messages = @room.messages.includes(:user)
+    calcmotion
+    # @point = Message.sum{ |hash| hash[:point]}
   end
 
   def create
     @room = Room.find(params[:room_id])
     @message = @room.messages.new(message_params)
+
     if @message.save
+
+      # binding.pry
       redirect_to room_messages_path(@room)
     else
       @messages = @room.messages.includes(:user)
@@ -16,9 +21,28 @@ class MessagesController < ApplicationController
     end
   end
 
+  def calcmotion
+    @point = Message.where(room_id: @room.id).sum(:motivation_id)
+    if @point == 0
+      @point = "普通です"
+    elsif @point >= 1
+      @point = "ハッピーです。楽しく過ごしましょう"
+    else
+      @point = "悲しいです。以下の曲を聴いて元気になりましょう"
+    end
+    # if @message.motivation_id.is_a?(Integer)
+    #   puts "数値"
+    # else
+    #   puts "数値じゃない"
+    # end
+  end
+
   private
 
   def message_params
     params.require(:message).permit(:content, :image, :motivation_id).merge(user_id: current_user.id)
   end
+
+
+
 end
